2aa06609f06b57a953e65df547c4a46a719f70a9
DianQK <xiaoqing@dianqk.org>
2015-12-30 00:07:05 +0800

    Add framework

---
+++ TransitionTreasury/TransitionTreasury.h
@@ -0,0 +1,19 @@
+//
+//  TransitionTreasury.h
+//  TransitionTreasury
+//
+//  Created by 宋宋 on 12/30/15.
+//  Copyright © 2015 TransitionTreasury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransitionTreasury.
+FOUNDATION_EXPORT double TransitionTreasuryVersionNumber;
+
+//! Project version string for TransitionTreasury.
+FOUNDATION_EXPORT const unsigned char TransitionTreasuryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitionTreasury/PublicHeader.h>
+
+
