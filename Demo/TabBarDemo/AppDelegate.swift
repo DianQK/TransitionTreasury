d61f0096045c126ee21f996f78afe7181190939c
DianQK <xiaoqing@dianqk.org>
2016-04-19 18:08:54 +0800

    import animation to Appdelegate

--- Demo/TabBarDemo/AppDelegate.swift
+++ Demo/TabBarDemo/AppDelegate.swift
@@ -8,23 +8,23 @@
 
 import UIKit
 import TransitionTreasury
+import TransitionAnimation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, TRTabBarControllerDelegate {
+	var window: UIWindow?
 
-    var window: UIWindow?
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		// Override point for customization after application launch.
+		if let tabBarController = window?.rootViewController as? UITabBarController {
+			tabBarController.tr_transitionDelegate = TRTabBarTransitionDelegate(method: TRTabBarTransitionMethod.Slide)
+			tabBarController.tr_delegate = self
+		}
+		return true
+	}
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        if let tabBarController = window?.rootViewController as? UITabBarController {
-            tabBarController.tr_transitionDelegate = TRTabBarTransitionDelegate(method: TRTabBarTransitionMethod.Slide)
-            tabBarController.tr_delegate = self
-        }
-        return true
-    }
-    
-    func tr_tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController) {
-        print("You did select \(viewController.dynamicType).")
-    }
+	func tr_tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController) {
+		print("You did select \(viewController.dynamicType).")
+	}
 }
 
